4a3eb94cc7fe4adcf8931497f80a4540e2c397c4
mahadidev7 <mahadidev7@gmail.com>
2022-12-16 19:13:52 +0600

    nft project setup complate

---
+++ screens/Datails.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Datails = () => {
+  return (
+    <View>
+      <Text>Datails</Text>
+    </View>
+  )
+}
+
+export default Datails
